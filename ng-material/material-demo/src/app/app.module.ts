aff085ba32c43f7e6b7773e862e8c9a1ce73b945
gpsakthivel <psakthi24vel@gmail.com>
2024-12-12 00:32:32 +0530

    Added material typography and material button

--- ng-material/material-demo/src/app/app.module.ts
+++ ng-material/material-demo/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from "./material/material.module";
+import { ButtonComponent } from './components/button/button.component';
+import { TypographyComponent } from './components/typography/typography.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    TypographyComponent
   ],
   imports: [
     BrowserModule,
